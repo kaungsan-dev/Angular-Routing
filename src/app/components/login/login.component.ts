486b7fb03bc0242206c8ab3b35508c47a9faa8e7
kaungsan-dev <kaungsandotcom@gmail.com>
2024-05-20 15:01:44 +0630

    merge

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['admin']);
+    }
+  }
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.auth.login(this.loginForm.value).subscribe(
+        (result) => {
+          console.log(result);
+          this.router.navigate(['/admin']);
+        },
+        (err: Error) => {
+          alert(err.message);
+        }
+      );
+    }
+  }
+}
